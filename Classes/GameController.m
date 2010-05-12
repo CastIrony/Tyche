7ffc69c17c5afdfbb9f044e8fbe935143d9ee0cc
Joel Bernstein <Bergamot@pdxelliotts02.webtrends.corp>
2010-05-11 20:16:06 -0700

    Version 1.0.204

--- Classes/GameController.m
+++ Classes/GameController.m
@@ -201,7 +201,7 @@
         
         [self.player.cards removeObject:card];
 
-        NSLog((NSString*)[card JSONRepresentation]);
+        NSLog(@"%@", [card JSONRepresentation]);
         
         [self.renderer.cardGroup discardCardWithSuit:card.suit numeral:card.numeral afterDelay:0.2 * i andThen:(card == lastCard ? work : nil)];
     }
