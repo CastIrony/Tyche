ea43e4c609e261d72d1e41e4b27f859b469064e6
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-05-11 17:27:41 -0700

    Version 1.0.178

--- Classes/GameController.m
+++ Classes/GameController.m
@@ -191,9 +191,9 @@
     for(uint i = 0; i < cards.count; i++)
     {
         GLCard* card = [cards objectAtIndex:i];
-        
-        [self.player.cards addObject:card];
 
+        if(![self.player.cards containsObject:card]) { [self.player.cards addObject:card]; }
+        
         [self.renderer.cardGroup dealCardWithSuit:card.suit numeral:card.numeral held:YES afterDelay:0.2 * i andThen:(card == lastCard ? work : nil)];
     }
 }
