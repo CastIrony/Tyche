5cfe0ec07af7296ef7045ddafa1e794b718fc494
Joel Bernstein <Bergamot@joels-macbook.ptp>
2010-05-17 20:23:45 -0700

    Version 1.0.241

--- GLTable.m
+++ GLTable.m
@@ -126,7 +126,7 @@
         
     GLfloat lightness = self.renderer.lightness.value;
     
-    glColor4f(lightness, lightness, lightness, 0.75);
+    glColor4f(lightness, lightness, lightness, 0.65);
     
     glVertexPointer  (3, GL_FLOAT, 0, topVertexArray);
     
