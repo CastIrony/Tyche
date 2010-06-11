8ee9b13772d5bff19054944c53c7b3211d85e39b
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-06-11 00:29:23 -0700

    Version 1.0.399

--- AnimatedFloat.h
+++ AnimatedFloat.h
@@ -19,17 +19,17 @@
 
 @property (nonatomic, assign)   GLfloat        startValue;
 @property (nonatomic, assign)   NSTimeInterval startTime;
-@property (nonatomic, copy)     simpleBlock          onStart;      
-@property (nonatomic, readonly) BOOL           hasStarted;
+@property (nonatomic, copy)     simpleBlock    onStart;      
+@property (nonatomic, assign)   BOOL           hasStarted;
 
 @property (nonatomic, assign)   GLfloat        endValue;
 @property (nonatomic, assign)   NSTimeInterval endTime;
-@property (nonatomic, copy)     simpleBlock          onEnd;    
-@property (nonatomic, readonly) BOOL           hasEnded;
+@property (nonatomic, copy)     simpleBlock    onEnd;    
+@property (nonatomic, assign)   BOOL           hasEnded;
 
 @property (nonatomic, assign)   AnimationCurve curve;
 
-@property (nonatomic, readonly)   GLfloat        value;
+@property (nonatomic, readonly) GLfloat      value;
 
 +(id)withValue:(GLfloat)value;
 +(id)withStartValue:(GLfloat)startValue endValue:(GLfloat)endValue speed:(GLfloat)speed;
