ff980ecdc602195bdf0c77bf0e963a64af578ee0
Joel Bernstein <Bergamot@joels-macbook.ptp>
2010-06-21 21:54:04 -0700

    Version 1.0.499

--- GLTexture.m
+++ GLTexture.m
@@ -314,6 +314,8 @@ static GLTexturePixelFormat defaultAlphaPixelFormat = kGLTexturePixelFormat_Defa
                 
                 CGRect position = CGRectMake(x + spacing - radius, y + spacing - radius, 2 * radius, 2 * radius);
                 
+                NSLog(@"Dot: x%f5 y%f5 w%f5 h%f5", position.x, position.y, position.width, position.height);
+                
                 CGContextAddEllipseInRect(context, position);
                 
                 CGContextFillPath(context);
