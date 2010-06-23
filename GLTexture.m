3f7c42ac4e7d1cab1c3444718e79641e2264fa51
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-06-22 23:16:55 -0700

    Version 1.0.622

--- GLTexture.m
+++ GLTexture.m
@@ -298,7 +298,7 @@ static GLTexturePixelFormat defaultAlphaPixelFormat = kGLTexturePixelFormat_Defa
         //  xx    xx    xx    xx    xx
         //           **    xx
         
-        CGContextSetLineWidth(context, 2);
+        CGContextSetLineWidth(context, 1);
         
         for(int i = 0; i < (dots / perRow); i++)
         {
