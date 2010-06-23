e104cf7f528c674dcc42e136dca651ff1dd54953
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-06-22 23:24:16 -0700

    Version 1.0.629

--- GLTexture.m
+++ GLTexture.m
@@ -333,7 +333,7 @@ static GLTexturePixelFormat defaultAlphaPixelFormat = kGLTexturePixelFormat_Defa
             int dot = (dots / perRow * perRow) + i;
             
             CGContextSetGrayFillColor  (context, 1.0, dot == current ? 0.5 : 0.0);
-            CGContextSetGrayStrokeColor(context, 1.0, dot == current ? 0.5 : 0.5);
+            CGContextSetGrayStrokeColor(context, 1.0, dot == current ? 0.8 : 0.5);
 
             GLfloat x = offset + i * spacing * 2;
             GLfloat y = (dots / perRow) * spacing * 2;
