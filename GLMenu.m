01728c5b59f3e6ea0d09ef969fa659db1589b628
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-06-22 18:26:43 -0700

    Version 1.0.554

--- GLMenu.m
+++ GLMenu.m
@@ -14,7 +14,7 @@
 
 @implementation GLMenu
 
-@synthesize texture        = _texture;
+//@synthesize texture        = _texture;
 @synthesize textController = _textController;
 @synthesize angleJitter    = _angleJitter;
 @synthesize angleSin       = _angleSin;
