a302f95967a6f4c3dc6c2297e665788d39bb1fb9
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-06-25 16:16:36 -0700

    Version 1.0.673

--- DisplayContainer.m
+++ DisplayContainer.m
@@ -1,6 +1,36 @@
 #import "DisplayContainer.h"
 
-
 @implementation DisplayContainer
+
+@synthesize keys;
+@synthesize objects;
+
++(DisplayContainer*)emptyContainer;
++(DisplayContainer*)containerWithKeys:(NSArray*)keys objectDictionary:(NSDictionary*)objects;
+
+-(DisplayContainer*)insertObject:(id)object asFirstWithKey:(id)key;
+-(DisplayContainer*)insertObject:(id)object asLastWithKey:(id)key;
+-(DisplayContainer*)insertObject:(id)object withKey:(id)key atIndex:(int)index;
+-(DisplayContainer*)insertObject:(id)object withKey:(id)key beforeKey:(id)before;
+-(DisplayContainer*)insertObject:(id)object withKey:(id)key afterKey:(id)after;
+
+-(DisplayContainer*)moveKeyToFirst:(id)key;
+-(DisplayContainer*)moveKeyToLast:(id)key;
+-(DisplayContainer*)moveKey:(id)key toIndex:(int)index;
+-(DisplayContainer*)moveKey:(id)key beforeKey:(id)before;
+-(DisplayContainer*)moveKey:(id)key afterKey:(id)after;
+
+-(DisplayContainer*)pruneObjectsForKey:(id)key toFormat:(NSString*)format;
+
+-(NSArray*)objectsForKey:(id)key;
+-(id)topObjectForKey:(id)key;
+
+-(NSEnumerator*)keyEnumerator;
+-(NSEnumerator*)objectEnumerator;
+-(NSEnumerator*)topObjectEnumerator;
+
+-(NSEnumerator*)reverseKeyEnumerator;
+-(NSEnumerator*)reverseObjectEnumerator;
+-(NSEnumerator*)reverseTopObjectEnumerator;
 
 @end
