137137cd3eaf41607e40eb85f53f81be725d17c7
Joel Bernstein <Bergamot@joels-macbook.ptp>
2010-06-28 20:09:18 -0700

    Version 1.0.781

--- Classes/GameRenderer.m
+++ Classes/GameRenderer.m
@@ -198,9 +198,9 @@
         [menu addServerWithPeerId:server name:server];
     }
     
-    //[self.menuLayerController pushMenu:menu forKey:@"main"];
+    [self.menuLayerController pushMenu:menu forKey:@"main"];
     
-    [self.menuLayerController pushMenu:[MenuControllerMain withRenderer:self] forKey:@"main"];
+    //[self.menuLayerController pushMenu:[MenuControllerMain withRenderer:self] forKey:@"main"];
     
     { GLChip* chip = [[[GLChip alloc] initWithChipNumber:0] autorelease]; chip.renderer = self; [self.chipGroup.chips setObject:chip forKey:@"1"]; }
     { GLChip* chip = [[[GLChip alloc] initWithChipNumber:1] autorelease]; chip.renderer = self; [self.chipGroup.chips setObject:chip forKey:@"5"]; }
