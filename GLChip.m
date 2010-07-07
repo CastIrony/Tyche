edc27e7f2bf3fa2dcde69cf128f37d7f51d94c8f
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-07-06 22:31:13 -0700

    Version 1.0.1012

--- GLChip.m
+++ GLChip.m
@@ -188,6 +188,8 @@
     stackVectors[offsetVector - 2].z -= 3 * (1 - fade);
     stackVectors[offsetVector - 1].z -= 3 * (1 - fade);
     
+    NSLog(@"%X - %X - %f5", self, self.chipGroup, self.chipGroup.opacity);
+    
     stackColors[offsetColors - 8] = Color3DMake(lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity,             fade * self.chipGroup.opacity);
     stackColors[offsetColors - 7] = Color3DMake(lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity,             fade * self.chipGroup.opacity);
     stackColors[offsetColors - 6] = Color3DMake(lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity, lightness * fade * self.chipGroup.opacity,             fade * self.chipGroup.opacity);
