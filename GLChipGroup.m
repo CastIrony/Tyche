dc25c5095b4200dabaea3cc34c9db926178a6dd5
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-07-06 22:18:47 -0700

    Version 1.0.1000

--- GLChipGroup.m
+++ GLChipGroup.m
@@ -16,15 +16,15 @@
     
     chipGroup.renderer = renderer;
 
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 0; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position +  6, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"1"    ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 8; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position - 18, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"10000"]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 1; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position +  3, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"5"    ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 7; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position - 15, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"2500 "]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 2; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position +  0, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"10"   ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 6; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position - 12, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"1000" ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 3; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position -  3, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"25"   ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 5; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position -  9, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"500"  ]; }
-    { GLChip* chip = [GLChip chip]; chip.chipNumber = 4; chip.chipGroup = chipGroup; chip.location = Vector3DMake(position -  6, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"100"  ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 0; chip.chipGroup = chipGroup; chip.location = Vector3DMake(  6, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"1"    ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 8; chip.chipGroup = chipGroup; chip.location = Vector3DMake(-18, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"10000"]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 1; chip.chipGroup = chipGroup; chip.location = Vector3DMake(  3, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"5"    ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 7; chip.chipGroup = chipGroup; chip.location = Vector3DMake(-15, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"2500 "]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 2; chip.chipGroup = chipGroup; chip.location = Vector3DMake(  0, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"10"   ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 6; chip.chipGroup = chipGroup; chip.location = Vector3DMake(-12, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"1000" ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 3; chip.chipGroup = chipGroup; chip.location = Vector3DMake( -3, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"25"   ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 5; chip.chipGroup = chipGroup; chip.location = Vector3DMake( -9, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"500"  ]; }
+    { GLChip* chip = [GLChip chip]; chip.chipNumber = 4; chip.chipGroup = chipGroup; chip.location = Vector3DMake( -6, 0, -4.3); [chipGroup.chips insertObject:chip asLastWithKey:@"100"  ]; }
     
     return chipGroup;
 }
