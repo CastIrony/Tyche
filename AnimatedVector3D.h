5e976d2bf4e5bf22ef17d01f1deba6bd085ae0f4
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-07-16 22:10:19 -0700

    Version 1.0.1206

--- AnimatedVector3D.h
+++ AnimatedVector3D.h
@@ -3,28 +3,15 @@
 #import "Geometry.h"
 
 @interface AnimatedVector3D : NSObject 
-{
-    Vector3D       _startValue;
-    NSTimeInterval _startTime;
-    simpleBlock          _onStart;      
-    BOOL           _hasStarted;
-    
-    Vector3D       _endValue;
-    NSTimeInterval _endTime;
-    simpleBlock          _onEnd;    
-    BOOL           _hasEnded;
-
-    AnimationCurve _curve;
-}
 
 @property (nonatomic, assign)   Vector3D       startValue;
 @property (nonatomic, assign)   NSTimeInterval startTime;
-@property (nonatomic, copy)     simpleBlock          onStart;      
+@property (nonatomic, copy)     simpleBlock    onStart;      
 @property (nonatomic, readonly) BOOL           hasStarted;
 
 @property (nonatomic, assign)   Vector3D       endValue;
 @property (nonatomic, assign)   NSTimeInterval endTime;
-@property (nonatomic, copy)     simpleBlock          onEnd;    
+@property (nonatomic, copy)     simpleBlock    onEnd;    
 @property (nonatomic, readonly) BOOL           hasEnded;
 
 @property (nonatomic, assign)   AnimationCurve curve;
@@ -33,8 +20,8 @@
 
 +(id)withValue:(Vector3D)value;
 
-+(id)withStartValue:(Vector3D)startValue endValue:(Vector3D)endValue speed:(GLfloat)speed;
 +(id)withStartValue:(Vector3D)startValue endValue:(Vector3D)endValue forTime:(NSTimeInterval)time;
++(id)withStartValue:(Vector3D)startValue endValue:(Vector3D)endValue speed:(GLfloat)speed;
 
 -(NSString*)description;
 
