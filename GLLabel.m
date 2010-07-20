36d9a861e94ffcf2303163a0bf8d4d8bbe0489be
Joel Bernstein <Bergamot@joels-macbook.ptp>
2010-07-19 22:38:02 -0700

    Version 1.0.1310

--- GLLabel.m
+++ GLLabel.m
@@ -575,8 +575,8 @@
 @dynamic isDead;
 @dynamic isAlive;
 
--(BOOL)isAlive { return within(self.death.value, 0, 0.001) && self.death.hasStarted; }
--(BOOL)isDead  { return within(self.death.value, 1, 0.001) && self.death.hasEnded; }
+-(BOOL)isAlive { return within(self.death.value, 0, 0.001); }
+-(BOOL)isDead  { return within(self.death.value, 1, 0.001); }
 
 -(void)killWithDisplayContainer:(DisplayContainer*)container andKey:(id)key
 {
