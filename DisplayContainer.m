6c9a7c1a3abe326a1e177595f690bae0611174d7
Joel Bernstein <Bergamot@Joels-MacBook.local>
2010-07-20 22:14:31 -0700

    Version 1.0.1320

--- DisplayContainer.m
+++ DisplayContainer.m
@@ -63,6 +63,11 @@
         
         id<Killable> topObject = [[self.hashtable objectForKey:key] lastObject];
         
+        if([key isEqualToString:@"bet"])
+        {
+            NSLog(@"bet is alive: %i", topObject.isAlive);
+        } 
+        
         if(topObject.isAlive)
         {
             [newLiveObjects addObject:topObject];
@@ -273,25 +278,6 @@
 
 -(void)pruneLiveForKey:(id)key
 {
-//    NSMutableArray* newLiveObjects = [[self.liveObjects mutableCopy] autorelease];
-//    NSMutableArray* newLiveKeys    = [[self.liveKeys    mutableCopy] autorelease];
-//        
-//    id<Killable> topObject = [[self.hashtable objectForKey:key] lastObject];
-//    
-//    if([key isEqualToString:@"bet"])
-//    {
-//        NSLog(@"bet is alive: %i", topObject.isAlive);
-//    }
-//    
-//    if(!topObject.isAlive)
-//    {
-//        [newLiveObjects removeObjectIdenticalTo:topObject];
-//        [newLiveKeys    removeObject:key];
-//    }
-//    
-//    self.liveObjects = newLiveObjects;
-//    self.liveKeys    = newLiveKeys;
-    
     [self generateObjectLists];
 }
 
