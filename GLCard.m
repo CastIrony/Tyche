31735c36918d10ee355c71a11b6eae916667e3e7
Joel Bernstein <Bergamot@joels-macbook.ptp>
2010-11-22 20:12:43 -0800

    Version 1.0.1466

--- GLCard.m
+++ GLCard.m
@@ -613,7 +613,7 @@
     GLfloat delta = pointTo.y - pointFrom.y;
 
     int start = 4 - ((pointFrom.y) / 96.0);
-    int target = 4 - ((delta) / 96.0) + start;
+    int target = 4 - ((delta) / 96.0) - start;
 
     LOG_NS(@"<%f5, %f5, %f5, %i>", pointTo.y, pointFrom.y, delta, target);
     
